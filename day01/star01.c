1704e0827eb9e1da24b015589485f54771025c84
jlazear <jlazear@gmail.com>
2022-12-01 19:20:44 -0800

    day01 in C

---
+++ day01/star01.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_ELVES 1000
+
+int read_input(char *fname, int *data) {
+    FILE *fp = fopen(fname, "r");
+    char line[40] = {0};
+    int n = 0;
+    int calories = 0;
+    int d = 0;
+
+    if (!fp) {
+        printf("Failed to open file %s!", fname);
+        exit(-1);
+    }
+
+    while (fgets(line, sizeof(line), fp)) {
+        if (line[0] == '\n' || line[0] == '\r') {
+            // printf("newline!\n");
+            n++;
+            data[n++] = calories;
+            calories = 0;
+        } 
+        else {
+            // printf("%s\n", line);
+            sscanf(line, "%d", &d);
+            calories += d;
+        }
+    }
+    if (calories) {
+        data[n++] = calories;
+    }
+    return n;
+}
+
+int cmpfunc(const void *a, const void* b) {
+    int aa = *((int*)a);
+    int bb = *((int*)b);
+
+    if (aa == bb) {
+        return 0;
+    } else {
+        return (aa > bb) ? 1 : -1;
+    }
+}
+
+int main(void) {
+    int elves[MAX_ELVES] = {0};
+    int n_elves = 0;
+
+    n_elves = read_input("input.txt", elves);
+    qsort(elves, n_elves, sizeof(int), cmpfunc);
+    printf("%d\n", elves[n_elves-1]);
+}
