5a21bd6c2c0f9d65a38859e9ad35bdf9f5d58b7b
jlazear <jlazear@gmail.com>
2022-12-04 20:58:59 -0800

    day04

---
+++ day04/star08.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int check_line(int x1, int x2, int y1, int y2) {
+    return (x1 <= y1 && y1 <= x2) || (y1 <= x1 && x1 <= y2);
+}
+
+int main(void) {
+    FILE *fp = fopen("input.txt", "r");
+    int x1, x2, y1, y2;
+    int n = 0;
+
+    while (fscanf(fp, "%d-%d,%d-%d\n", &x1, &x2, &y1, &y2) == 4) {
+        if (check_line(x1, x2, y1, y2)) n++;
+    }
+
+    printf("n = %d\n", n);
+}
---
+++ day04/star08.py
@@ -0,0 +1,15 @@
+def check_line(line):
+    line = line.strip()
+    xs, ys = line.split(',')
+    x1, x2 = map(int, xs.split('-'))
+    y1, y2 = map(int, ys.split('-'))
+
+    return (x1 <= y1 <= x2) or (y1 <= x1 <= y2)
+
+n = 0
+with open('input.txt') as f:
+    for line in f:
+        if check_line(line):
+            n += 1
+
+print(f"{n = }")
