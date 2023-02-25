f4422a5572c87dbaa00a870fe810cbf224ced193
jlazear <jlazear@gmail.com>
2023-02-24 23:09:45 -0800

    day06 c

---
+++ day06/star12.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_STRLEN 10000
+#define SEQ_LEN 14
+
+int check(char line[], int left, int seq_len) {
+    int letters[26] = {0};
+    char c = 0;
+    for (int i=0; i<seq_len; i++) {
+        c = line[left + i];
+        if (letters[c - 'a']++) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int find_sequence(char line[], int seq_len) {
+    int left = 0;
+    while (!check(line, left, seq_len)) {
+        left++;
+    }
+    return left + seq_len;
+}
+
+int main(void) {
+    FILE *fp = fopen("input.txt", "r");
+    char line[MAX_STRLEN] = {0};
+    
+    fscanf(fp, "%s", line);
+
+    printf("%d", find_sequence(line, SEQ_LEN));
+
+}
